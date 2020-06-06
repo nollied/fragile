109a2890ffc1df30cb992066d5a2b7edb5ea0873
guillemdb <guillem@fragile.tech>
2020-06-06 11:47:57 +0200

    Minor fixes. Improve memory

--- fragile/core/memory.py
+++ fragile/core/memory.py
@@ -1,21 +1,15 @@
 import logging
-from typing import Iterable, List, Tuple, Union
+from typing import Dict, Iterable, List, Tuple, Union
 
-from fragile.backend import dtype, tensor, typing
+from fragile.backend import Backend, random_state, tensor, typing
 from fragile.core.swarm import Swarm
 
 
 class ReplayMemory:
-    """Store replay data extracted from a :class:`HistoryTree`."""
-
-    _log = logging.getLogger("Replaymemory")
+    _log = logging.getLogger("Memory")
 
     def __init__(
-        self,
-        max_size: int,
-        names: Union[List[str], Tuple[str]],
-        mode: str = "best",
-        min_size: int = None,
+        self, max_size: int, names: Union[List[str], Tuple[str]], min_size: int = None,
     ):
         """
         Initialize a :class:`ReplayMemory`.
@@ -23,10 +17,6 @@ class ReplayMemory:
         Args:
             max_size: Maximum number of experiences that will be stored.
             names: Names of the replay data attributes that will be stored.
-            mode: If ``mode == "best"`` store only data from the best trajectory \
-                  of the :class:`Swarm`. Otherwise store data from all the states of \
-                  the :class:`HistoryTree`.
-
             min_size: Minimum number of samples that need to be stored before the \
                      replay memory is considered ready. If ``None`` it will be equal \
                      to max_size.
@@ -35,7 +25,6 @@ class ReplayMemory:
         self.max_size = max_size
         self.min_size = max_size if min_size is None else min_size
         self.names = names
-        self.mode = mode
         self.reset()
 
     def __len__(self):
@@ -68,20 +57,34 @@ class ReplayMemory:
         """
         return len(self) >= self.min_size
 
-    def memorize(self, swarm: Swarm):
+    def get_values(self) -> Tuple[typing.Tensor, ...]:
+        """Return a tuple containing the memorized data for all the saved data attributes."""
+        return tuple([getattr(self, val) for val in self.names])
+
+    def as_dict(self) -> Dict[str, typing.Tensor]:
+        return dict(zip(self.names, self.get_values()))
+
+    def iterate_values(self, randomize: bool = False) -> Iterable[Tuple[typing.Tensor]]:
         """
-        Extract the replay data from a :class:`Swarm` and incorporate it to the \
-        already saved experiences.
+        Return a generator that yields a tuple containing the data of each state \
+        stored in the memory.
         """
-        # extract data from the swarm
-        if self.mode == "best":
-            data = next(swarm.tree.iterate_branch(swarm.best_id, batch_size=-1, names=self.names))
-        else:
-            data = next(swarm.tree.iterate_nodes_at_random(batch_size=-1, names=self.names))
-        # Concatenate the data to the current memory
-        for name, val in zip(self.names, data):
+        indexes = range(len(self))
+        if randomize:
+            with Backend.use_backend("numpy"):
+                indexes = random_state.permutation(indexes)
+        for i in indexes:
+            yield tuple([getattr(self, val)[i] for val in self.names])
+
+    def append(self, **kwargs):
+        for name, val in kwargs.items():
+            if name not in self.names:
+                raise KeyError("%s not in self.names: %s" % (name, self.names))
             # Scalar vectors are transformed to columns
-            if dtype.is_tensor(val) and len(val.shape) == 1:
+            val = tensor.to_backend(val)
+            if len(val.shape) == 0:
+                val = tensor.unsqueeze(val)
+            if len(val.shape) == 1:
                 val = val.reshape(-1, 1)
             processed = (
                 val
@@ -93,14 +96,57 @@ class ReplayMemory:
             setattr(self, name, processed)
         self._log.info("Memory now contains %s samples" % len(self))
 
-    def get_values(self) -> Tuple[typing.Tensor, ...]:
-        """Return a tuple containing the memorized data for all the saved data attributes."""
-        return tuple([getattr(self, val) for val in self.names])
 
-    def iterate_values(self) -> Iterable[Tuple[typing.Tensor]]:
+class SwarmMemory(ReplayMemory):
+    """Store replay data extracted from a :class:`HistoryTree`."""
+
+    def __init__(
+        self,
+        max_size: int,
+        names: Union[List[str], Tuple[str]],
+        mode: str = "best",
+        min_size: int = None,
+    ):
         """
-        Return a generator that yields a tuple containing the data of each state \
-        stored in the memory.
+        Initialize a :class:`ReplayMemory`.
+
+        Args:
+            max_size: Maximum number of experiences that will be stored.
+            names: Names of the replay data attributes that will be stored.
+            mode: If ``mode == "best"`` store only data from the best trajectory \
+                  of the :class:`Swarm`. Otherwise store data from all the states of \
+                  the :class:`HistoryTree`.
+
+            min_size: Minimum number of samples that need to be stored before the \
+                     replay memory is considered ready. If ``None`` it will be equal \
+                     to max_size.
+
         """
-        for i in range(len(self)):
-            yield tuple([getattr(self, val)[i] for val in self.names])
+        super(ReplayMemory, self).__init__(max_size=max_size, min_size=min_size, names=names)
+        self.mode = mode
+
+    def append_swarm(self, swarm: Swarm):
+        """
+        Extract the replay data from a :class:`Swarm` and incorporate it to the \
+        already saved experiences.
+        """
+        # extract data from the swarm
+        if self.mode == "best":
+            data = next(swarm.tree.iterate_branch(swarm.best_id, batch_size=-1, names=self.names))
+        else:
+            data = next(swarm.tree.iterate_nodes_at_random(batch_size=-1, names=self.names))
+        self.append(**dict(zip(self.names, data)))
+        # Concatenate the data to the current memory
+        """for name, val in zip(self.names, data):
+            # Scalar vectors are transformed to columns
+            if dtype.is_tensor(val) and len(val.shape) == 1:
+                val = val.reshape(-1, 1)
+            processed = (
+                val
+                if getattr(self, name) is None
+                else tensor.concatenate([val, getattr(self, name)])
+            )
+            if len(processed) > self.max_size:
+                processed = processed[: self.max_size]
+            setattr(self, name, processed)"""
+        self._log.info("Memory now contains %s samples" % len(self))
